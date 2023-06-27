61646e3db4600797f23c5e4b1a99c599b4699eb1
Halim Choi <hchoicareer@gmail.com>
2023-06-27 14:37:49 -0400

    refactored auth form

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card'
 import { useHistory } from 'react-router-dom'
 
 function Auth({ updateUser }) {
@@ -65,103 +66,111 @@ function Auth({ updateUser }) {
 
     return (
         <>
-            <div className="auth-form">
-                <div className="auth-info">
-                    <h2 style={{ color: 'red' }}>{formErrors ? renderFormErrors() : null}</h2>
-                    <h2>Please Log in or Sign up!</h2>
-                    <h2>{signUp ? 'Already a member?' : 'Not a member?'}</h2>
-                    <Button variant="secondary" onClick={handleClick}>
-                        {signUp ? 'Log In!' : 'Register now!'}
-                    </Button>
-                </div>
-                <Form style={{ width: '25rem' }} onSubmit={userLoginOrCreation}>
-                    {signUp ? (
-                        <div>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control
-                                    type="username"
-                                    name="username"
-                                    placeholder="Enter username"
-                                    value={formState.username}
-                                    onChange={changeFormState}
-                                    required
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    name="email"
-                                    placeholder="Enter email URL"
-                                    value={formState.email}
-                                    onChange={changeFormState}
-                                    required
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicAddress">
-                                <Form.Label>
-                                    Address
-                                    <Form.Text className="text-muted" style={{ display: "flex", justifyContent: 'center' }}>
-                                        We'll never share your address with anyone else.
-                                    </Form.Text>
-                                </Form.Label>
-                                <Form.Control
-                                    type="default_address"
-                                    name="default_address"
-                                    placeholder="e.g. 12345 Street Rd, City State Zipcode"
-                                    value={formState.default_address}
-                                    onChange={changeFormState}
-                                    required
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={formState.password}
-                                    onChange={changeFormState}
-                                    required
-                                />
-                            </Form.Group>
+            <Card bg="dark" text="light" style={{ maxWidth: '35rem', margin: '0 auto', marginTop: '30px' }}>
+                <Card.Body>
+                    <div className="auth-form d-flex flex-column align-items-center">
+                        <div className="auth-info">
+                            <div className="d-flex flex-column align-items-center">
+                                <h2 style={{ color: 'red' }}>{formErrors ? renderFormErrors() : null}</h2>
+                                <h2 className="text-center">Please Log in or Sign up!</h2>
+                                <h2 className="text-center">{signUp ? 'Already a member?' : 'Not a member?'}</h2>
+                                <Button variant="secondary" onClick={handleClick}>
+                                    {signUp ? 'Log In!' : 'Register now!'}
+                                </Button>
+                            </div>
                         </div>
-                    ) : (
-                        <div>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control
-                                    type="username"
-                                    name="username"
-                                    placeholder="Enter username"
-                                    value={formState.username}
-                                    onChange={changeFormState}
-                                    required
-                                />
-                            </Form.Group>
+                        <Form style={{ width: '20rem' }} onSubmit={userLoginOrCreation}>
+                            {signUp ? (
+                                <div>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control
+                                            type="username"
+                                            name="username"
+                                            placeholder="Enter username"
+                                            value={formState.username}
+                                            onChange={changeFormState}
+                                            required
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            name="email"
+                                            placeholder="Enter email URL"
+                                            value={formState.email}
+                                            onChange={changeFormState}
+                                            required
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicAddress">
+                                        <Form.Label>
+                                            Address
+                                            <Form.Text className="text-muted" style={{ display: "flex", justifyContent: 'center' }}>
+                                                We'll never share your address with anyone else.
+                                            </Form.Text>
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="default_address"
+                                            name="default_address"
+                                            placeholder="e.g. 12345 Street Rd, City State Zipcode"
+                                            value={formState.default_address}
+                                            onChange={changeFormState}
+                                            required
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            name="password"
+                                            placeholder="Password"
+                                            value={formState.password}
+                                            onChange={changeFormState}
+                                            required
+                                        />
+                                    </Form.Group>
+                                </div>
+                            ) : (
+                                <div>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control
+                                            type="username"
+                                            name="username"
+                                            placeholder="Enter username"
+                                            value={formState.username}
+                                            onChange={changeFormState}
+                                            required
+                                        />
+                                    </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={formState.password}
-                                    onChange={changeFormState}
-                                    required
-                                />
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            name="password"
+                                            placeholder="Password"
+                                            value={formState.password}
+                                            onChange={changeFormState}
+                                            required
+                                        />
+                                    </Form.Group>
+                                </div>
+                            )}
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="I'm not a robot" required />
                             </Form.Group>
-                        </div>
-                    )}
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="I'm not a robot" required />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
+                            <div className="d-flex justify-content-center">
+                                <Button variant="success" type="submit">
+                                    {signUp ? 'Login' : 'Register'}
+                                </Button>
+                            </div>
+                        </Form>
+                    </div>
+                </Card.Body>
+            </Card >
         </>
     );
 }
